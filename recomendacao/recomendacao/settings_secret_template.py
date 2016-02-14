50feae05170b36cd7e3b2c6929ef5e2f40e211e1
alissonmuller <alissonmller@gmail.com>
2016-02-14 21:00:30 -0200

    settings_secret_template.py

---
+++ recomendacao/recomendacao/settings_secret_template.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+
+
+# Fill the settings below with the correct values
+# and copy this file to "settings_secret.py"
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'unique random string'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.example.com'
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER = 'user@example.com'
+EMAIL_HOST_PASSWORD = 'password'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+MANAGERS = ADMINS
+SERVER_EMAIL = 'django@example.com'
